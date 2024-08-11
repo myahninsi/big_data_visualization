b3c800f36ea34e8abbb2632611ac51404487127b
Alexander Ramos <157079260+MixedCuy@users.noreply.github.com>
2024-08-11 15:35:31 -0400

    Add file
    Updated new functions and libraries

--- predictor.py
+++ predictor.py
@@ -5,9 +5,32 @@ matplotlib.use('Agg')           # Select Matplolib Backend
 import matplotlib.pyplot as plt
 import numpy as np              # type: ignore
 import math
+import os
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error # type: ignore
 
-def load_model_and_scaler(model_path='pickle/linear_regression_model.pkl', scaler_path='pickle/minmax_scaler.pkl'):
+
+def data_files_names():                                     #this function serve to display in a dropdown button on the HTML
+    data_path='data'
+    file_names=[]
+    for file in os.listdir(data_path):
+        f=os.path.join(data_path,file)                      #obtaining the path of the file
+        if os.path.isfile(f):           
+            file_names.append(file)                         #appending the name of the files
+    return file_names
+
+def determine_model(name):                                  #return the path to work with the models
+    if name=="Multiple Linear Regression":
+        return "pickle/multiple_linear_regression/linear_regression_model.pkl"
+    elif name=="Multiple Linear Regression with lasso regularization":
+        return "pickle/multiple_linear_regression/linear_regression_lasso_model.pkl"
+    elif name=="Multiple Linear Regression with ridge regularization":
+        return "pickle/multiple_linear_regression/linear_regression_ridge_model.pkl"
+    elif name=="Multiple Linear Regression with elastic net regularization":
+        return "pickle/multiple_linear_regression/linear_regression_elasticnet_model.pkl"
+    else:
+        return "pickle/multiple_linear_regression/linear_regression_model.pkl" #in case none of the options is selected, use multiple linear regression by default
+
+def load_model_and_scaler(model_path, scaler_path='pickle/scaler/minmax_scaler.pkl'):
     model = joblib.load(model_path)
     scaler = joblib.load(scaler_path)
     return model, scaler
@@ -33,7 +56,6 @@ def evaluate_model(y_new, y_pred):
 def plot_predictions_vs_actuals(y_new, y_pred):
     #plt.figure(figsize=(10, 5))
     plt.scatter(y_new, y_pred,label="Predictions")
-    #plt.plot(y_new,y_pred,color="red",linestyle="--",label="Perfect fit")
     plt.xlabel("Actual")
     plt.ylabel("Predicted")
     plt.title('Actual vs Predicted Values')
