f9fabd3d6bd7c28eb8ac444cad0a07bcc7db1a8a
Alexander Ramos <157079260+MixedCuy@users.noreply.github.com>
2024-08-14 15:14:33 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask,render_template,request,redirect,jsonify
 import pandas as pd # type: ignore
 import os
-from predictor import load_model_and_scaler, predict_prices, evaluate_model,plot_predictions_vs_actuals,determine_model,data_files_names
+from predictor import predict_prices,determine_model,data_files_names,zipcode_detail
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')                                                 #displays the housing price prediction view
 def home():
-    file_names=data_files_names()
-    return render_template("index.html",file_names=file_names)
+    zipcodes=zipcode_detail()                                   #loading the zip codes with its labels
+    return render_template("model.html",zipcodes=zipcodes)
 
 @app.route('/visualize')
 def dashboard():
@@ -23,20 +23,40 @@ def get_data(filename):
     return jsonify({'headers':headers,'rows':rows})         #for dynamic update of the table it returns a json to use it with javscript in the HTML
 
 @app.route('/predict',methods=['POST'])
-def predict():
-    mrg_name = request.form.get('selectedOption')           #obtains the name of multiple linear regression (mrg) to be executed
-    fn=request.form.get('selectedOption2')                  #obtains the name of the file (fn) in this case csv
-    model_path=determine_model(mrg_name)                    #retrieving the path of the respective model
-    model, scaler = load_model_and_scaler(model_path)       #retrieving the multiple linear regression model and scaler
-    #test_data = pd.read_csv('data/test.csv')               #loading a dataset  
-    data = pd.read_csv('data/{}'.format(fn))
-    y_new,y_pred = predict_prices(model, scaler,data)       #returning the actual, and predicted dependent variable
-    mse,rmse,mae,r2 = evaluate_model(y_new, y_pred)         #return evaluation metrics
-    metrics={"mse":mse,"rmse":rmse,"mae":mae,"r2":r2}
-    predictions=True
-    plot_predictions_vs_actuals(y_new,y_pred)               #just indicates that the predictions were performed and allow to display a section in HTML
-    return render_template("index.html", headings=data.columns.tolist(), data=data.values.tolist(), predictions=predictions,metrics=metrics)
+def predict_price():
+    data = {
+        'bedrooms': [int(request.form.get('bedrooms', 0))],
+        'bathrooms': [int(request.form.get('bathrooms', 0))],
+        'sqft_living': [float(request.form.get('sqft_living', 0.0))],
+        'sqft_lot': [float(request.form.get('sqft_lot', 0.0))],
+        'floors': [int(request.form.get('floors', 0))],
+        'waterfront': [int(request.form.get('waterfront', 0))],
+        'view': [int(request.form.get('view', 0))],
+        'condition': [int(request.form.get('condition', 0))],
+        'grade': [int(request.form.get('grade', 0))],
+        'sqft_basement': [float(request.form.get('sqft_basement', 0.0))],
+        'yr_built': [int(request.form.get('yr_built', 0))],
+        'sqft_lot15': [float(request.form.get('sqft_lot15', 0.0))],
+        'Renovated': [int(request.form.get('renovated', 0))],
+        'zipcode_label': [int(request.form.get('zipcode', 0))] 
+    }
+    
+    df=pd.DataFrame(data)                       #converting the data entered by the user as a pandas data frame      
+    mlr_name=request.form.get('regression')     #obtaines the name of the multiple linear regression technique
+    model,r2,rmse=determine_model(mlr_name)     #retrieves the model and its r2 and rmse
+    r2=round(r2,2)
+    rmse=round(rmse,2)
+    predicted_price=predict_prices(model,df)                #retrieves the price based on the model selected
+    print(predicted_price)
+    
+    if mlr_name in ("mlr","mlr_ridge"):
+        predicted_price=round(predicted_price[0][0],2)
+    else:
+        predicted_price=round(predicted_price[0],2)
+    
+    return render_template("model.html",predictions=True,predicted_price=predicted_price,r2=r2,rmse=rmse,mlr_name=mlr_name)
 
+#not in use - maybe eliminated for the last presentation
 @app.route('/upload',methods=['POST'])
 def upload_file():                                          #saves a file (csv)
     file=request.files['file']
